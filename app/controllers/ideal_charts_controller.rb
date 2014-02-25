de1518f536aea2fdb5a6d54182a0a5c92448fb48
SylvieC <sylviecottrell@gmail.com>
2014-02-24 17:03:52 -0800

    changes in ideal-charts_controllers

--- app/controllers/ideal_charts_controller.rb
+++ app/controllers/ideal_charts_controller.rb
@@ -5,6 +5,10 @@ class IdealChartsController < ApplicationController
     @idealcharts = IdealChart.all
    end
 
+   def new
+    @idealchart = Idealchart.new
+   end
+
    def create
     new_user_id = params[:user_id] 
     new_idealchart = params.require(:ideal_chart).permit(:health_score, :social_score, :intellect_score)
@@ -17,9 +21,7 @@ class IdealChartsController < ApplicationController
     end
    end
 
-   def new
-    @idealchart = Idealchart.new
-   end
+  
 
    def update
     @idealchart = IdealChart.find(params[:id])
@@ -32,8 +34,12 @@ class IdealChartsController < ApplicationController
    end
 
    def show
+     @idealchart =IdealChart.find(params[:id])
+    respond_to do |format|
+      format.html
+      format.json {render json: @idealchart}
+    end
+     redirect_to user_dashboard_path(current_user.id)
    end
-
-
 
 end
